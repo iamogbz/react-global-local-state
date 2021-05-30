1d7f2544303186040580bdad763ac18e20bb0251
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2021-05-30 04:09:37 -0400

    Document useValue hook

--- src/hooks/useValue.ts
+++ src/hooks/useValue.ts
@@ -2,42 +2,62 @@ import * as React from "react";
 import * as moment from "moment";
 import isEqual from "lodash.isequal";
 
+/** Utility type for possible undefined return value */
 type MaybeValue<T> = T | undefined;
+
+/** Function that returns a value or undefined */
 interface ValueGetter<T> {
   (): MaybeValue<T>;
 }
 
+/**
+ * Alias for type which is either a getter or value.
+ * See {@link React.useState}.
+ */
 type Valuer<T> = MaybeValue<T> | ValueGetter<T>;
+
+/**
+ * Return a plain value from either function or given value.
+ */
 function getValue<T>(valuer: Valuer<T>): MaybeValue<T>;
 function getValue(valuer: unknown) {
   if (typeof valuer === "function") return valuer();
   return valuer;
 }
 
+/** Function to get timestamp for a variable */
 type ValueStamper<R> = (...args: never[]) => R;
+/** Utility type to extract the stamp type from a stamper function */
 type Stamp<U> = (U extends ValueStamper<infer R> ? R : number) | number;
+/**
+ * Return the result of stamping using the stamper or falling back to
+ * {@link moment.now} if no valid stamper is provided.
+ */
 function getStamp<T>(stamper: T): Stamp<T> {
   if (typeof stamper === "function") return stamper();
   return moment.now();
 }
 
+/** Utility type for running some code on a plain value */
 export interface OnValue<T> {
   (newValue?: T): unknown;
 }
 
+/** Utility type for objects with value resolver setter */
 interface UpdateableValue<T> {
   value: Valuer<T>;
   setValue: OnValue<T>;
 }
 
+/** Params for `useValue` which include the updateable initial value and time stamper */
 interface UseValueOptions<T, U> {
-  source?: Partial<UpdateableValue<T>>;
+  initial?: Partial<UpdateableValue<T>>;
   stamper?: U;
 }
 
-interface UseValueResult<T, U> {
+/** Results from `useValue` which includes the recent value, time stamp and updater */
+interface UseValueResult<T, U> extends UpdateableValue<T> {
   value: MaybeValue<T>;
-  setValue: OnValue<T>;
   stamp: Stamp<U>;
 }
 
@@ -46,7 +66,7 @@ interface UseValueResult<T, U> {
  * Uses {@link lodash.isequal} to compare values when checking for a change.
  */
 export function useValue<T, U>({
-  source: initial,
+  initial,
   stamper,
 }: UseValueOptions<T, U> = {}): UseValueResult<T, U> {
   const [state, setState] = React.useState(() => ({
@@ -70,19 +90,28 @@ export function useValue<T, U>({
   return { ...state, setValue };
 }
 
+/**
+ * Params for `useTrackedValue` which include the source value, stamper and a predicate.
+ */
 interface UseTrackedValueOptions<T, U> {
   source: Partial<UseValueResult<T, U>>;
   stamper?: U;
   shouldUpdate: (sourceStamp?: Stamp<U>, targetStamp?: Stamp<U>) => boolean;
 }
 
+/**
+ * This keeps track of the source value and updates the internal value using the
+ * given update predicate method. This compares the stamp of the incoming source
+ * and the internal value, updating the tracked internal value which is returned
+ * if there is a change from the incoming source.
+ */
 export function useTrackedValue<T, U>({
   source,
   stamper,
   shouldUpdate,
 }: UseTrackedValueOptions<T, U>): UseValueResult<T, U> {
   const tracked = useValue({
-    source,
+    initial: source,
     stamper,
   });
 
@@ -101,6 +130,11 @@ export function useTrackedValue<T, U>({
   return tracked;
 }
 
+/**
+ * Sync a parent with a dependent child value. The incoming parent source updates are
+ * always tracked, while the child value which is returned can be updated independently
+ * only being overwritten by the parent source if there is a more recent stamped value.
+ */
 export function useSyncedValue<T, U>({
   source,
 }: Pick<UseTrackedValueOptions<T, U>, "source">): UseValueResult<T, U> {
